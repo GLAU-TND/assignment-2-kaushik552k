7bbc0b673ea98a83e44665279c8d47eaf8e5370d
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-03-28 01:42:08 +0530

    getter and setter method for front variable

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -10,4 +10,12 @@ import problem3.node.Node;
 
 public class MyPriorityQueue {
     private Node front;
+
+    public Node getFront(){
+        return front;
+    }
+
+    public void setFront(Node front){
+        this.front = front;
+    }
 }
