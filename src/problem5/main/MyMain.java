9546641c6b3aff7af2d02e92a420a340b8200df4
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-03-28 16:08:43 +0530

    taking the value of nodes from the user

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,11 +6,16 @@
  */
 package problem5.main;
 
+import problem5.circularqueue.MyCircularQueue;
+
 import java.util.Scanner;
 
 //executable class
 public class MyMain {
     public static void main(String[] args){
         Scanner sc = new Scanner( System.in );
+        MyCircularQueue circularQueue = new MyCircularQueue();
+        System.out.println( "Enter number of records you wants to enter" );
+        int numberOfRecords = sc.nextInt();
     }
 }
