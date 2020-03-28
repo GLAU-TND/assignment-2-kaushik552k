74f4ccebb007698a522bfb1f57e52833c1458376
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-03-28 16:14:17 +0530

    while loop for taking user input and assigning it into student queue

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -7,6 +7,8 @@
 package problem5.main;
 
 import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
+import problem5.student.Student;
 
 import java.util.Scanner;
 
@@ -17,5 +19,27 @@ public class MyMain {
         MyCircularQueue circularQueue = new MyCircularQueue();
         System.out.println( "Enter number of records you wants to enter" );
         int numberOfRecords = sc.nextInt();
+        while(numberOfRecords != 0) {
+            Student student = new Student();
+            Node node = new Node();
+
+            System.out.println( "Enter name" );
+            student.setName( sc.next().trim() );
+            System.out.println( "Enter roll number" );
+            student.setRollNumber( sc.nextInt() );
+            System.out.println( "Enter backlogs" );
+            int backLogs = sc.nextInt();
+            if(backLogs > 2) {
+                System.out.println( "This student is not allowed" );
+                continue;
+            }
+            student.setBackLogCounter( backLogs );
+
+            node.setStudent( student );
+
+            circularQueue.enQueue( node );
+
+            numberOfRecords--;
+        }
     }
 }
